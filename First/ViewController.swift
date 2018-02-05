fc6a2db3f1065375e3bcf5116a1daf37890d62be
Ryan Liao <ryanliaoyz@ryans-mac-mini.local>
2018-02-05 08:14:09 -0500

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Ryan Liao on 2/5/18.
+//  Copyright © 2018 Ryan Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
