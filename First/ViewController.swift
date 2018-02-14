34d6af285929b4bfd9fa3f1f24fe1beaaa9fe2ba
Ryan Liao <ryanliaoyz@ryans-mac-mini.local>
2018-02-14 00:55:29 -0500

    1

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
